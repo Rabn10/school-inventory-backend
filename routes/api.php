a50f3698f8fa519b270f57001fbc6de9fe5065a9
Rabin <rabin.awale09@gmail.com>
2025-06-24 16:18:44 +0545

    Feature: api to login user

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/register', [AuthorizationController::class, 'userRegister']);
+Route::post('/login', [AuthorizationController::class, 'login']);
